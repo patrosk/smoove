6c07c591f5a4e2f841fbc496b29d3b31ab78dfae
patrosk <naiade99@gmail.com>
2021-11-24 17:59:01 +0100

    Fixed dropdown menu items in footer

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const menuOpen = document.querySelector('.menu');
-const menuClosed = document.querySelector('.close');
-const listBox = document.querySelector('.list-box');
+// NAVBAR MENU
+const menuOpen = document.querySelector(".menu");
+const menuClosed = document.querySelector(".close");
+const listBox = document.querySelector(".list-box");
 
-menuOpen.addEventListener('click', () => {
-    listBox.classList.add('menu-down');
+menuOpen.addEventListener("click", () => {
+  listBox.classList.add("menu-down");
+});
+
+menuClosed.addEventListener("click", () => {
+  listBox.classList.remove("menu-down");
+});
+
+// FOOTER DROPDOWN
+const listItems = document.querySelectorAll(".footer-menu li");
+
+listItems.forEach((listItem) => {
+  const button = listItem.querySelector("button");
+  const dropdown = listItem.querySelector(".dropdown");
+
+  console.log(dropdown);
+  console.log(button);
+
+  button.addEventListener("click", () => {
+    dropdown.classList.toggle("show-dropdown");
   });
-
-  menuClosed.addEventListener('click', () => {
-    listBox.classList.remove('menu-down');
-  });
-
-
-
+});
