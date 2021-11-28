b3afa255272fc03aecd6de1921de43746c34edab
patrosk <naiade99@gmail.com>
2021-11-28 15:03:06 +0100

    added desktop images, media queries and choose light function

--- app.js
+++ app.js
@@ -13,25 +13,71 @@ menuClosed.addEventListener("click", () => {
 });
 
 const menuGoAway = document.querySelector(".menu-down");
-document.addEventListener('scroll', () => {
+document.addEventListener("scroll", () => {
   navMenu.classList.remove("menu-down");
-})
+});
 
-
-
-
-
-menuOpen.addEventListener('click', () => {
-  nav.classList.add("nav-black")
+menuOpen.addEventListener("click", () => {
+  nav.classList.add("nav-black");
 });
 
 menuClosed.addEventListener("click", () => {
   nav.classList.remove("nav-black");
 });
 
+// CHOOSE LIGHT
+const image = document.querySelector(".color .light");
+const greenButton = document.querySelector(".green");
+const lilacButton = document.querySelector(".lilac");
+const pinkButton = document.querySelector(".pink");
 
+greenButton.addEventListener("click", () => {
+  image.src = "./images/sideviewGREEN.jpg";
+  greenButton.classList.toggle("chosen");
+  pinkButton.classList.remove("chosen");
+  lilacButton.classList.remove("chosen");
+});
 
+lilacButton.addEventListener("click", () => {
+  image.src = "./images/sideviewLILAC.jpg";
+  lilacButton.classList.toggle("chosen");
+  pinkButton.classList.remove("chosen");
+  greenButton.classList.remove("chosen");
+});
 
+pinkButton.addEventListener("click", () => {
+  image.src = "./images/sideviewPINK.jpg";
+  pinkButton.classList.toggle("chosen");
+  lilacButton.classList.remove("chosen");
+  greenButton.classList.remove("chosen");
+});
+
+// CHOOSE LIGHT DESKTOP
+const imageDesktop = document.querySelector(".color .light-desktop");
+const greenButtonDesktop = document.querySelector(".green-desktop");
+const lilacButtonDesktop = document.querySelector(".lilac-desktop");
+const pinkButtonDesktop = document.querySelector(".pink-desktop");
+
+greenButtonDesktop.addEventListener("click", () => {
+  imageDesktop.src = "./images/sideviewGREEN.jpg";
+  greenButtonDesktop.classList.toggle("chosen");
+  pinkButtonDesktop.classList.remove("chosen");
+  lilacButtonDesktop.classList.remove("chosen");
+});
+
+lilacButtonDesktop.addEventListener("click", () => {
+  imageDesktop.src = "./images/sideviewLILAC.jpg";
+  lilacButtonDesktop.classList.toggle("chosen");
+  pinkButtonDesktop.classList.remove("chosen");
+  greenButtonDesktop.classList.remove("chosen");
+});
+
+pinkButtonDesktop.addEventListener("click", () => {
+  imageDesktop.src = "./images/sideviewPINK.jpg";
+  pinkButtonDesktop.classList.toggle("chosen");
+  lilacButtonDesktop.classList.remove("chosen");
+  greenButtonDesktop.classList.remove("chosen");
+});
 
 // FOOTER DROPDOWN
 const listItems = document.querySelectorAll(".footer-menu li");
@@ -40,11 +86,7 @@ listItems.forEach((listItem) => {
   const button = listItem.querySelector("button");
   const dropdown = listItem.querySelector(".dropdown");
 
-  console.log(dropdown);
-  console.log(button);
-
   button.addEventListener("click", () => {
     dropdown.classList.toggle("show-dropdown");
   });
 });
-
